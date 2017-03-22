246fdf1fe5cd1fbb7c6f046442959ed8be2cdc60
praveendareddy21 <praveendareddy@gmail.com>
2017-03-22 14:15:53 -0700

    tf with mnist data using sigmoid activation example

---
+++ tf_mnist_sigmoid.py
@@ -0,0 +1,25 @@
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+import numpy as np
+from random import Random
+from tensorflow.contrib import rnn
+from tensorflow.python.framework import ops
+
+def init_weights(shape):
+    return tf.Variable(tf.random_normal(shape, stddev=0.01))
+
+
+def model(X, w_h, w_o):
+    h = tf.nn.sigmoid(tf.matmul(X, w_h)) # this is a basic mlp, think 2 stacked logistic regressions
+    return tf.matmul(h, w_o) # note that we dont take the softmax at the end because our cost fn does that for us
+
+
+mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+trX, trY, teX, teY = mnist.train.images, mnist.train.labels, mnist.test.images, mnist.test.labels
+
+X = tf.placeholder("float", [None, 784])
+Y = tf.placeholder("float", [None, 10])
+
+w_h = init_weights([784, 625]) # create symbolic variables
+w_o = init_weights([625, 10])
+
